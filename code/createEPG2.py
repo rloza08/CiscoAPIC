718de3245745f05dbdcdb7c7ab939dd242141c04
Russel Gil Lozano <rloza08@safeway.com>
2021-03-16 10:18:25 +0800

    2

--- code/createEPG2.py
+++ code/createEPG2.py
@@ -1,24 +1,21 @@
 import os
 import yaml
-import json
 import requests
-# import time
 from getLoginToken import loginToken
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 current_directory = os.path.dirname(os.path.realpath(__file__))
 with open(current_directory + "\\config.yaml", "r") as yamlfile:
-	config = yaml.load(yamlfile, Loader=yaml.FullLoader)
+    config = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
 loginToken = loginToken()
-# time.sleep(1)
 
 # Variables
-	# Application Profile
+# 	Application Profile
 tenant = "az-acitest-nonprod-02"
 appProfileName = "Russel-Provider-AppProfile-39-01"
-	# EPG
+# 	EPG
 cloudEpgName = "Russel-EPG-Provider-39-01"
 matchExpression = "custom:role=='web',custom:snet=='ngcp-qa-snet',custom:environment=='qa2',custom:appcode=='ngcp'"
 cloudEPSelectorName = "Russel-EndpointSelector-39-01"
@@ -28,14 +25,14 @@ vrf = "nonprod"
 url = config['url'] + "/api/node/mo/uni/tn-" + tenant + "/cloudapp-" + appProfileName + ".json"
 headers = {'Content-type': 'application/json', 'Accept': '*/*', 'Cookie': 'APIC-cookie=' + loginToken}
 payload = {
-	"cloudApp": {
-		"attributes": {
-			"descr": appProfileName,
-			"name": appProfileName,
-			"status": "created"
-		},
-		"children": []
-	}
+    "cloudApp": {
+        "attributes": {
+            "descr": appProfileName,
+            "name": appProfileName,
+            "status": "created"
+        },
+        "children": []
+    }
 }
 
 r = requests.post(url, headers=headers, json=payload, verify=False)
@@ -43,38 +40,38 @@ rjson = r.json()
 
 print(rjson)
 
-
-### Create EPG ###
-url = config['url'] + "/api/node/mo/uni/tn-" + tenant + "/cloudapp-" + appProfileName + "/cloudepg-" + cloudEpgName + ".json"
+# Create EPG
+url = config[
+          'url'] + "/api/node/mo/uni/tn-" + tenant + "/cloudapp-" + appProfileName + "/cloudepg-" + cloudEpgName + ".json"
 headers = {'Content-type': 'application/json', 'Accept': '*/*', 'Cookie': 'APIC-cookie=' + loginToken}
 payload = {
-	"cloudEPg": {
-		"attributes": {
-			"name": cloudEpgName,
-			"status": "created"
-		},
-		"children": [
-			{
-				"cloudEPSelector": {
-					"attributes": {
-						"matchExpression": matchExpression,
-						"name": cloudEPSelectorName,
-						"status": "created"
-					},
-					"children": []
-				}
-			},
-			{
-				"cloudRsCloudEPgCtx": {
-					"attributes": {
-						"status": "",
-						"tnFvCtxName": vrf
-					},
-					"children": []
-				}
-			}
-		]
-	}
+    "cloudEPg": {
+        "attributes": {
+            "name": cloudEpgName,
+            "status": "created"
+        },
+        "children": [
+            {
+                "cloudEPSelector": {
+                    "attributes": {
+                        "matchExpression": matchExpression,
+                        "name": cloudEPSelectorName,
+                        "status": "created"
+                    },
+                    "children": []
+                }
+            },
+            {
+                "cloudRsCloudEPgCtx": {
+                    "attributes": {
+                        "status": "",
+                        "tnFvCtxName": vrf
+                    },
+                    "children": []
+                }
+            }
+        ]
+    }
 }
 
 r = requests.post(url, headers=headers, json=payload, verify=False)
